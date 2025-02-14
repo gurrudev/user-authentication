ae32415c6461453b2a9e1ab85cabdcc89bb401b2
gurrudev <pawarash000@outlook.com>
2025-02-14 17:06:22 +0530

    UPDATE: User Memoized

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserData } from "../api/endpoints";
 import Navbar from "../components/Navbar";
@@ -11,6 +11,8 @@ const Home = () => {
     const token = localStorage.getItem("token");
     const navigate = useNavigate();
     const { user, setUser } = useContext(UserContext);
+
+    const memoizedUser = useMemo(() => user, [user]);
 
     // console.log(user);
     useEffect(() => {
@@ -32,22 +34,22 @@ const Home = () => {
 
     return (
         <>
-            <Navbar user={user && user.firstName} />
+            <Navbar user={memoizedUser && memoizedUser.firstName} />
             <div className="max-w-sm mx-auto bg-white rounded-xl mt-20 shadow-md overflow-hidden border border-gray-200">
                 <div className="flex flex-col items-center p-6">
                     <img
-                        src={user && user.avatar}
+                        src={memoizedUser && memoizedUser.avatar}
                         alt="User Avatar"
                         className="w-24 h-24 rounded-full border-2 border-gray-300"
                     />
                     <h2 className="mt-4 text-lg font-semibold text-gray-900">
-                        {user && user.firstName + " " + user.lastName}
+                        {memoizedUser && memoizedUser.firstName + " " + memoizedUser.lastName}
                     </h2>
                     <p className="text-sm text-gray-600 capitalize">
-                        {user && user.gender}
+                        {memoizedUser && memoizedUser.gender}
                     </p>
                     <p className="text-sm text-gray-500">
-                        {user && user.email}
+                        {memoizedUser && memoizedUser.email}
                     </p>
                 </div>
             </div>
