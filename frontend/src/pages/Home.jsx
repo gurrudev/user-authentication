4cec1d5b5136f8024b79d1663a245db10a04bfce
gurrudev <pawarash000@outlook.com>
2025-02-14 19:04:14 +0530

    UPDATE: user context

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,5 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-unused-vars */
-import { useState, useEffect, useContext, useMemo } from "react";
+import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserData } from "../api/endpoints";
 import Navbar from "../components/Navbar";
 import UserContext from "../context/userContext";
 import ProfileCardSkeleton from "../components/ProfileCardSkeleton";
@@ -10,9 +7,7 @@ import ProfileCardSkeleton from "../components/ProfileCardSkeleton";
 const Home = () => {
     const token = localStorage.getItem("token");
     const navigate = useNavigate();
-    const { user, setUser, isLoading } = useContext(UserContext);
-
-    const memoizedUser = useMemo(() => user, [user]);
+    const { user, isLoading } = useContext(UserContext);
 
     useEffect(() => {
         if (!token) {
@@ -23,28 +18,25 @@ const Home = () => {
 
     return (
         <>
-            <Navbar user={isLoading ? "" : memoizedUser && memoizedUser.firstName} />
+            <Navbar user={isLoading ? "" : user && user.firstName} />
             <div className="max-w-sm mx-auto bg-white rounded-xl mt-20 shadow-md overflow-hidden border border-gray-200">
                 {isLoading ? (
                     <ProfileCardSkeleton />
                 ) : (
                     <div className="flex flex-col items-center p-6">
                         <img
-                            src={memoizedUser && memoizedUser.avatar}
+                            src={user && user.avatar}
                             alt="User Avatar"
                             className="w-24 h-24 rounded-full border-2 border-gray-300"
                         />
                         <h2 className="mt-4 text-lg font-semibold text-gray-900">
-                            {memoizedUser &&
-                                memoizedUser.firstName +
-                                    " " +
-                                    memoizedUser.lastName}
+                            {user && user.firstName + " " + user.lastName}
                         </h2>
                         <p className="text-sm text-gray-600 capitalize">
-                            {memoizedUser && memoizedUser.gender}
+                            {user && user.gender}
                         </p>
                         <p className="text-sm text-gray-500">
-                            {memoizedUser && memoizedUser.email}
+                            {user && user.email}
                         </p>
                     </div>
                 )}
